877b51ca8678d31c2ad8d650cea09302960158ee
Aroon Narayanan <aroon@gatech.edu>
2020-09-03 19:24:53 -0400

    Updates

--- precache-manifest.8a0947ef683fbefc4395190bec30deb2.js
+++ precache-manifest.8a0947ef683fbefc4395190bec30deb2.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9186b584969148b3c375b29d957e4ba9",
+    "revision": "9a524d9142f6520d12ecc7e99cd2b5e5",
     "url": "/index.html"
   },
   {
-    "revision": "1889ea561f876bc73567",
+    "revision": "2046729b3148f3974de8",
     "url": "/static/css/2.fc5a9450.chunk.css"
   },
   {
-    "revision": "7893aa58635b2110d368",
+    "revision": "8840437412f48f7f4356",
     "url": "/static/css/main.7014cfad.chunk.css"
   },
   {
-    "revision": "1889ea561f876bc73567",
-    "url": "/static/js/2.0de37c25.chunk.js"
+    "revision": "2046729b3148f3974de8",
+    "url": "/static/js/2.603135d2.chunk.js"
   },
   {
     "revision": "d0550c944a9928585a31b6efc1a5d942",
-    "url": "/static/js/2.0de37c25.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.603135d2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7893aa58635b2110d368",
-    "url": "/static/js/main.ecf74d6f.chunk.js"
+    "revision": "8840437412f48f7f4356",
+    "url": "/static/js/main.50c1c988.chunk.js"
   },
   {
     "revision": "30a127fe480c10d8ec04",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.116c8735e595c12548d0d7d1985d9951.js"
+  "/precache-manifest.8a0947ef683fbefc4395190bec30deb2.js"
 );
 
 self.addEventListener('message', (event) => {
